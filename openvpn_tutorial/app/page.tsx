048167363564b25559b8672ca50c3323f078b642
monkeyark <monkeyark@gmail.com>
2024-12-01 04:20:53 -0600

    update favicon

--- openvpn_tutorial/app/page.tsx
+++ openvpn_tutorial/app/page.tsx
@@ -1,9 +1,3 @@
-// import { OpenVPNTutorial } from "@/components/openvpn-tutorial"
-
-// export default function Page() {
-//   return <OpenVPNTutorial />
-// }
-
 import OpenVPNTutorial from "@/components/openvpn-tutorial"
 
 export default function Home() {
