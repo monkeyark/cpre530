e5c54070dbe93854260c2730892e9b09baf27702
monkeyark <monkeyark@gmail.com>
2024-12-01 03:52:24 -0600

    add webpage

---
+++ openvpn_tutorial/app/page.tsx
@@ -0,0 +1,12 @@
+// import { OpenVPNTutorial } from "@/components/openvpn-tutorial"
+
+// export default function Page() {
+//   return <OpenVPNTutorial />
+// }
+
+import OpenVPNTutorial from "@/components/openvpn-tutorial"
+
+export default function Home() {
+  return <OpenVPNTutorial />
+}
+
